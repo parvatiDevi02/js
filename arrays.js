2b52157e28bd527db9dd64a672fecb9200cd35bb
parvatiDevi02 <devi061973@gmail.com>
2025-11-02 16:50:36 +0530

    first commit

--- arrays.js
+++ arrays.js
@@ -1,20 +1,20 @@
-/*let parvathi = [10,20,30,40,5,60];
+let parvathi = [10,20,30,40,5,60];
 const val1 = parvathi.find(num => num<40);
-console.log(val1);*/
-/*let parvathi = [10,20,30,40,5,60];
-const val1 = parvathi.filter(num => num<40);
-console.log(val1);*/
-/*let parvathi = [10,20,30,40,5,60];
-const val1 = parvathi.map(num => num<40);
-console.log(val1);*/
+console.log(val1);
+let parvathi2 = [10,20,30,40,5,60];
+const val2 = parvathi.filter(num => num<40);
+console.log(val1);
+let parvathi3 = [10,20,30,40,5,60];
+const val3 = parvathi.map(num => num<40);
+console.log(val1);
 //spread operator[...]
-/*var arr1=[1,2,3,4];
+var arr1=[1,2,3,4];
 var arr2=[...arr1];
-console.log(arr1);*/
-/*var arr1=[1,2,3,4];
+console.log(arr1);
+var arr1=[1,2,3,4];
 var arr2=[0,9,8,7];
 var arr2=[...arr1];
-console.log(arr1);*/
+console.log(arr1);
 const arr3 = [100,200]
 const arr4 = [...arr3,300,400]
 console.log(arr4);
--- dom.js
+++
@@ -1,4 +0,0 @@
-// getelement by id
-// getelement by class
-//getelement by tag
-//doucument=html code
